77289d1b82abda918bbc55c5a4fdaa3641c21eb1
Artur <artur.sienicki@gmail.com>
2019-12-29 00:34:08 +0100

    changed resx type

--- MailingService.ExternalLib/EmailTemplates.Designer.cs
+++ MailingService.ExternalLib/EmailTemplates.Designer.cs
@@ -22,7 +22,7 @@ namespace MailingService.ExternalLib {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class EmailTemplates {
+    public class EmailTemplates {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace MailingService.ExternalLib {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MailingService.ExternalLib.EmailTemplates", typeof(EmailTemplates).Assembly);
@@ -51,7 +51,7 @@ namespace MailingService.ExternalLib {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -63,7 +63,7 @@ namespace MailingService.ExternalLib {
         /// <summary>
         ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
-        internal static byte[] Hello {
+        public static byte[] Hello {
             get {
                 object obj = ResourceManager.GetObject("Hello", resourceCulture);
                 return ((byte[])(obj));
